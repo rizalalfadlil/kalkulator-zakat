d2847507fd38b10ccc9ae41a5aadd881001a3679
RizalAlfadlil <rizalalfadlil@gmail.com>
2025-10-28 19:32:26 +0700

    z

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-white sticky top-0 p-8 shadow-sm">
+    <header className="bg-white sticky top-0 p-8 z-20 shadow-sm">
       <p className="font-medium text-sm">UTS Logika Matematika</p>
       <p className="text-2xl font-bold">Kalkulator Zakat</p>
     </header>
